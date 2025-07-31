6786bb3b31c5e3072445fd5683dcf6612d71cdb0
henryathel <henry@athelas.co.uk>
2025-07-31 20:36:51 +0100

    break

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,15 @@
+import {describe, expect, test} from "vitest";
+import { getAPIKey } from "src/api/auth";
+
+type thingy = {
+    header: string
+}
+
+describe("test_getAPIKey", () => {
+    test("this works", () => {
+        const pp = {header: "oi"}
+        const outcome = getAPIKey(pp
+        expect(outcome).toBeNull
+    })
+}
+)
