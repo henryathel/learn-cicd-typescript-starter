6e2549be707d40e05c0c1a3bfef75327922bd632
henryathel <henry@athelas.co.uk>
2025-08-03 18:47:07 +0100

    some formatting

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,15 +1,14 @@
-import {describe, expect, test} from "vitest";
+import { describe, expect, test } from "vitest";
 import { getAPIKey } from "src/api/auth";
 
 type thingy = {
-    header: string
-}
+  header: string;
+};
 
 describe("test_getAPIKey", () => {
-    test("this works", () => {
-        const pp = {header: "oi"}
-        const outcome = getAPIKey(pp)
-        expect(outcome).toBeNull
-    })
-}
-)
+  test("this works", () => {
+    const pp = { header: "oi" };
+    const outcome = getAPIKey(pp);
+    expect(outcome).toBeNull;
+  });
+});
